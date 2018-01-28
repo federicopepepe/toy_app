4c2a68aa00fd36fc8317b0bb137b7a17233ae10b
sakyo <tksakyy.0108@example.com>
2018-01-28 15:43:22 +0000

    finish toy app

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+    has_many:microposts
+    validates FILL_IN,presence:true # [FILL_IN]をコードに置き換えてください
+    validates FILL_IN,presence:true # [FILL_IN]をコードに置き換えてください
+    
+end
